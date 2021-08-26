ce7e527a1436461097de4de1af75533563a3f906
Danilo Bezerra <danilo.silva.b@gmail.com>
2021-08-26 14:56:21 -0300

    Added path to Chrome webdriver version 90

--- bot_pdf_download.py
+++ bot_pdf_download.py
@@ -2,7 +2,11 @@ from selenium import webdriver
 import selenium
 # from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome("C:\\Users\\danil\OneDrive\\Dev Repository\\Packages-Resorces\\chromedriver.exe")
+# To use in personnal laptop
+# driver = webdriver.Chrome("C:\\Users\\danil\OneDrive\\Dev Repository\\Packages-Resorces\\chromedriver.exe")
+
+# To use in LATAM's laptop
+driver = webdriver.Chrome("C:\\Users\\danilo.bezerra\\Documents\\Dev Resources\\chromedriver.exe")
 
 urlToAirbusWorld = 'https://w3.airbus.com/H380/world/airbusworld/forms/airbus.sfcc?TYPE=33554433&REALMOID=06-653aefcb-c624-47d3-bc0a-85721530460f&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-t%2fTX88CtTFIPqQ15ugBfOME66PQGCWGuAAOSByMZUiVIJPozo431pS4LE4YuSGzU&TARGET=-SM-https%3a%2f%2fw3%2eairbus%2ecom%2fnewairbusworld'
 urlLinktoDocument = 'https://w3.airbus.com/1H43/MEFO_AW/TFU/MzYuMDAuMDAuMDA5/article.html'
