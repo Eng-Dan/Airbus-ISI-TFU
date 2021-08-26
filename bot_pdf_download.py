f64df70deab962f4a86f784b5108cad2ed12ffe7
Danilo Bezerra <danilo.silva.b@gmail.com>
2021-08-26 17:15:15 -0300

    Added code to login with user and password

--- bot_pdf_download.py
+++ bot_pdf_download.py
@@ -1,19 +1,33 @@
+'''Selenium documentation at https://selenium-python.readthedocs.io/installation.html'''
+
 from selenium import webdriver
-import selenium
-# from selenium.webdriver.common.keys import Keys
-
-# To use in personnal laptop
-# driver = webdriver.Chrome("C:\\Users\\danil\OneDrive\\Dev Repository\\Packages-Resorces\\chromedriver.exe")
-
-# To use in LATAM's laptop
-driver = webdriver.Chrome("C:\\Users\\danilo.bezerra\\Documents\\Dev Resources\\chromedriver.exe")
+from selenium.webdriver.common.keys import Keys
+import time
 
 urlToAirbusWorld = 'https://w3.airbus.com/H380/world/airbusworld/forms/airbus.sfcc?TYPE=33554433&REALMOID=06-653aefcb-c624-47d3-bc0a-85721530460f&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-t%2fTX88CtTFIPqQ15ugBfOME66PQGCWGuAAOSByMZUiVIJPozo431pS4LE4YuSGzU&TARGET=-SM-https%3a%2f%2fw3%2eairbus%2ecom%2fnewairbusworld'
-urlLinktoDocument = 'https://w3.airbus.com/1H43/MEFO_AW/TFU/MzYuMDAuMDAuMDA5/article.html'
+urlLinktoDocument = 'https://w3.airbus.com/1H43/MEFO_AW/TFU/MzYuMTEuMDAxMDY=/article.html'
+
+# userName = input("Enter your login: ")
+# userPass = input("Enter your password: ")
+# login = SAO_danilobs
+# pass = 'Aib20211'
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+# Path to use in personnal laptop
+# driver = webdriver.Chrome("C:\\Users\\danil\OneDrive\\Dev Repository\\Packages-Resorces\\chromedriver.exe")
+
+# Path to use in LATAM's laptop
+driver = webdriver.Chrome("C:\\Users\\danilo.bezerra\\Documents\\Dev Resources\\chromedriver.exe", options=options)
 
 driver.get(urlLinktoDocument)
 
-# userName = 'SAO_danilobs'
-# passWord = 'Aib20211'
+loginField = driver.find_element_by_name("USER")
+passwordField = driver.find_element_by_name("PASSWORD")
 
-# C:\Users\danil\Programming\anaconda3\Scripts\pip.exe install selenium
+loginField.send_keys("SAO_danilobs")
+passwordField.send_keys("Aib20211")
+passwordField.send_keys(Keys.RETURN)
+
+#driver.close()
