5f3083389a64af45a7badee0149052af9243ae12
Danilo Bezerra <danilo.silva.b@gmail.com>
2021-08-26 09:33:55 -0300

    Code for opening a web page

---
+++ bot_pdf_download.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import selenium
+# from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome("C:\\Users\\danil\OneDrive\\Dev Repository\\Packages-Resorces\\chromedriver.exe")
+
+urlToAirbusWorld = 'https://w3.airbus.com/H380/world/airbusworld/forms/airbus.sfcc?TYPE=33554433&REALMOID=06-653aefcb-c624-47d3-bc0a-85721530460f&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-t%2fTX88CtTFIPqQ15ugBfOME66PQGCWGuAAOSByMZUiVIJPozo431pS4LE4YuSGzU&TARGET=-SM-https%3a%2f%2fw3%2eairbus%2ecom%2fnewairbusworld'
+urlLinktoDocument = 'https://w3.airbus.com/1H43/MEFO_AW/TFU/MzYuMDAuMDAuMDA5/article.html'
+
+driver.get(urlLinktoDocument)
+
+# userName = 'SAO_danilobs'
+# passWord = 'Aib20211'
+
+# C:\Users\danil\Programming\anaconda3\Scripts\pip.exe install selenium
