1ae91cf01d995ef9a0713314dd2bfdc88bdd4f65
Danilo Bezerra <danilo.silva.b@gmail.com>
2021-08-27 10:11:04 -0300

    Added code for bot partial operation

--- bot_pdf_download.py
+++ bot_pdf_download.py
@@ -1,7 +1,10 @@
 '''Selenium documentation at https://selenium-python.readthedocs.io/installation.html'''
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common import action_chains
 from selenium.webdriver.common.keys import Keys
+import keyboard
 import time
 
 urlToAirbusWorld = 'https://w3.airbus.com/H380/world/airbusworld/forms/airbus.sfcc?TYPE=33554433&REALMOID=06-653aefcb-c624-47d3-bc0a-85721530460f&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-t%2fTX88CtTFIPqQ15ugBfOME66PQGCWGuAAOSByMZUiVIJPozo431pS4LE4YuSGzU&TARGET=-SM-https%3a%2f%2fw3%2eairbus%2ecom%2fnewairbusworld'
@@ -29,5 +32,26 @@ passwordField = driver.find_element_by_name("PASSWORD")
 loginField.send_keys("SAO_danilobs")
 passwordField.send_keys("Aib20211")
 passwordField.send_keys(Keys.RETURN)
+
+time.sleep(5)
+documentWindowHandle = driver.current_window_handle
+
+saveButton = driver.find_element_by_xpath('/html/body/div/div[1]/table/tbody/tr[1]/td[2]/a/img')
+
+actions = ActionChains(driver)
+actions.move_to_element(saveButton)
+actions.click()
+actions.perform()
+
+for window_handle in driver.window_handles:
+        if window_handle != documentWindowHandle:
+            driver.switch_to.window(window_handle)
+            break
+
+time.sleep(1)
+
+keyboard.press(['ctrl', 's'])
+time.sleep(0.5)
+keyboard.press('enter')
 
 #driver.close()
