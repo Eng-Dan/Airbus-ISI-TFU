0c8c0b173af4948ed434a2914b2b4cbac690dc56
Danilo Bezerra <danilo.silva.b@gmail.com>
2021-08-27 15:18:49 -0300

    Code refactored

--- bot_pdf_download.py
+++ bot_pdf_download.py
@@ -1,19 +1,13 @@
-'''Selenium documentation at https://selenium-python.readthedocs.io/installation.html'''
-
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-from selenium.webdriver.common import action_chains
 from selenium.webdriver.common.keys import Keys
 import keyboard
 import time
 
-urlToAirbusWorld = 'https://w3.airbus.com/H380/world/airbusworld/forms/airbus.sfcc?TYPE=33554433&REALMOID=06-653aefcb-c624-47d3-bc0a-85721530460f&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-t%2fTX88CtTFIPqQ15ugBfOME66PQGCWGuAAOSByMZUiVIJPozo431pS4LE4YuSGzU&TARGET=-SM-https%3a%2f%2fw3%2eairbus%2ecom%2fnewairbusworld'
 urlLinktoDocument = 'https://w3.airbus.com/1H43/MEFO_AW/TFU/MzYuMTEuMDAxMDY=/article.html'
 
-# userName = input("Enter your login: ")
-# userPass = input("Enter your password: ")
-# login = SAO_danilobs
-# pass = 'Aib20211'
+userName = input("Enter your login: ")
+userPass = input("Enter your password: ")
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -29,12 +23,11 @@ driver.get(urlLinktoDocument)
 loginField = driver.find_element_by_name("USER")
 passwordField = driver.find_element_by_name("PASSWORD")
 
-loginField.send_keys("SAO_danilobs")
-passwordField.send_keys("Aib20211")
-passwordField.send_keys(Keys.RETURN)
-
-time.sleep(5)
 documentWindowHandle = driver.current_window_handle
+
+loginField.send_keys(userName)
+passwordField.send_keys(userPass)
+passwordField.send_keys(Keys.RETURN)
 
 saveButton = driver.find_element_by_xpath('/html/body/div/div[1]/table/tbody/tr[1]/td[2]/a/img')
 
@@ -51,7 +44,9 @@ for window_handle in driver.window_handles:
 time.sleep(1)
 
 keyboard.press(['ctrl', 's'])
-time.sleep(0.5)
+time.sleep(1)
 keyboard.press('enter')
 
-#driver.close()
+time.sleep(20)
+
+driver.quit()
