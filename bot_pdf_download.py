2309f713593d98566bdc2815638f9240e90e0ca5
Danilo Bezerra <danilo.silva.b@gmail.com>
2021-09-01 11:28:21 -0300

    Coded bot using python context management (with statement)

--- bot_pdf_download.py
+++ bot_pdf_download.py
@@ -4,61 +4,54 @@ from selenium.webdriver.common.keys import Keys
 import keyboard
 import time
 
-def activate_bot(documentUrl, AirbusUserName, AirbusUserPass):
-    print("Bot over file at", documentUrl)
-
-    urlLinktoDocument = documentUrl
-
+def run_bot_pdf_download(documentUrl, AirbusUserName, AirbusUserPass):
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    # Path to use in personnal laptop
-    # driver = webdriver.Chrome("C:\\Users\\danil\OneDrive\\Dev Repository\\Packages-Resorces\\chromedriver.exe")
+    with webdriver.Chrome("C:\\Users\\danilo.bezerra\\Documents\\Dev Resources\\chromedriver.exe", options=options) as driver:
+        print("Getting PDF...")
+        driver.implicitly_wait(10)
 
-    # Path to use in LATAM's laptop
-    driver = webdriver.Chrome("C:\\Users\\danilo.bezerra\\Documents\\Dev Resources\\chromedriver.exe", options=options)
+        driver.get(documentUrl)
 
-    driver.get(urlLinktoDocument)
+        loginField = driver.find_element_by_name("USER")
+        passwordField = driver.find_element_by_name("PASSWORD")
 
-    loginField = driver.find_element_by_name("USER")
-    passwordField = driver.find_element_by_name("PASSWORD")
+        documentWindowHandle = driver.current_window_handle
 
-    documentWindowHandle = driver.current_window_handle
-
-    loginField.send_keys(AirbusUserName)
-    passwordField.send_keys(AirbusUserPass)
-    passwordField.send_keys(Keys.RETURN)
-
-    try:
-        saveButton = driver.find_element_by_xpath('//*[@id="save"]/a')
-    except:
-        print('--Save button not found by XPath = //*[@id="save"]/a')
-
+        loginField.send_keys(AirbusUserName)
+        passwordField.send_keys(AirbusUserPass)
+        passwordField.send_keys(Keys.RETURN)
+        
         try:
-            saveButton = driver.find_element_by_xpath('/html/body/div/div[1]/table/tbody/tr[1]/td[2]/a/img')
+            saveButton = driver.find_element_by_xpath('//*[@id="save"]/a')
         except:
-            print('--Save button not found by XPath = /html/body/div/div[1]/table/tbody/tr[1]/td[2]/a/img')
-            print("--No files downloaded")
-            driver.quit()
-                                            
-    actions = ActionChains(driver)
-    actions.move_to_element(saveButton)
-    actions.click()
-    actions.perform()
+            print('-- Error: Save button not found by XPath = //*[@id="save"]/a')
 
-    for window_handle in driver.window_handles:
-            if window_handle != documentWindowHandle:
-                driver.switch_to.window(window_handle)
-                break
+            try:
+                saveButton = driver.find_element_by_xpath('/html/body/div/div[1]/table/tbody/tr[1]/td[2]/a/img')
+            except:
+                print('-- Error: Save button not found by XPath = /html/body/div/div[1]/table/tbody/tr[1]/td[2]/a/img')
+                print("   No files downloaded")
+                driver.quit()
 
-    time.sleep(1)
+        actions = ActionChains(driver)
+        actions.move_to_element(saveButton)
+        actions.click()
+        actions.perform()
 
-    keyboard.press(['ctrl', 's'])
-    time.sleep(1)
-    keyboard.press('enter')
+        for window_handle in driver.window_handles:
+                if window_handle != documentWindowHandle:
+                    driver.switch_to.window(window_handle)
+                    break
+                
+        time.sleep(2)
 
-    time.sleep(5)
-    
-    print("--PDF file downloaded\n")
+        keyboard.press(['ctrl', 's'])
 
-    driver.quit()
+        time.sleep(1)
+
+        keyboard.press('enter')
+        print("  Download launched")
+
+        time.sleep(10)
